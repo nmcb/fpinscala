3a6896eab32d788e66eb6a991608556d5eddefb0
nmcb <marco.borst@splatter.nl>
2015-07-13 11:48:28 +0200

    updated until 6.7

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -75,13 +75,37 @@ trait Stream[+A] {
     case _                                              => false
   }
 
-  // from answers, interwoven:
+  def tails: Stream[Stream[A]] = unfold(this) {
+    case Cons(h, t) => Some((cons(h(), t()), t()))
+    case _          => None
+  } append(Stream(Empty))
+
+  // Scoped the answers for ScanRight
+  //
+  // The function can't be implemented using `unfold`, since `unfold` generates elements
+  // of the `Stream` from left to right. It can be implemented using `foldRight` though.
+  // The implementation is just a `foldRight` that keeps the accumulated value and the
+  // stream of intermediate results, which we `cons` onto during each iteration. When
+  // writing folds, it's common to have more state in the fold than is needed to compute
+  // the result. Here, we simply extract the accumulated list once finished.
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p0) => {
+      // p0 is passed by-name and used in by-name args in f and cons.
+      // So use lazy val to ensure only one evaluation...
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
+
+  def hasSubsequence[A](s: Stream[A]): Boolean = tails exists (_ startsWith s)
+
+  // from answers, interweaves:
   // - the zipping,
   // - the taking while s not exhausted (breaking out early),
-  // - and the validation that each (optional, including this) head equals the other
+  // - and the validation that each head equals the other
   def startsWithInterwoven[A](s: Stream[A]): Boolean =
     zipAll(s).takeWhile(!_._2.isEmpty) forAll {
-      case (h,h2) => h == h2
+      case (h, h2) => h == h2
     }
 
   def mapByUnfold[B](f: A => B): Stream[B] = unfold(this) {
--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -1,5 +1,6 @@
 package fpinscala.state
 
+import scala.annotation.tailrec
 
 trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
@@ -7,6 +8,78 @@ trait RNG {
 
 object RNG {
   // NB - this was called SimpleRNG in the book text
+
+  type Rand[+A] = RNG => (A, RNG)
+  val randInt: Rand[Int] = _.nextInt
+  val randNonNegative: Rand[Int] = map(randInt)(i => if (i < 0) -(i + 1) else i)
+  val randNonNegativeEven: Rand[Int] = map(randNonNegative)(i => i - i % 2)
+  val randomBoolean: Rand[Boolean] = map(randInt)(i => i % 2 == 0)
+  val randDouble: Rand[Double] = map(randInt)(i => i / (Int.MaxValue.toDouble + 1))
+  val randIntDouble: Rand[(Int, Double)] = both(randInt, randDouble)
+  val randDoubleInt: Rand[(Double, Int)] = both(randDouble, randInt)
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+  }
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+  def boolean(rng: RNG): (Boolean, RNG) = {
+    val (i, r) = rng.nextInt
+    (i % 2 == 0, r)
+  }
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((n, d), r) = intDouble(rng)
+    ((d, n), r)
+  }
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (n, r0) = rng.nextInt
+    val (d, r1) = double(r0)
+    ((n, d), r1)
+  }
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d0, r0) = double(rng)
+    val (d1, r1) = double(r0)
+    val (d2, r2) = double(r1)
+    ((d0, d1, d2), r2)
+  }
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, r) = rng.nextInt
+    (n / (Int.MaxValue.toDouble + 1), r)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @tailrec
+    def loop(n: Int, r: RNG, a: List[Int]): (List[Int], RNG) = {
+      if (n == 0) (a, r)
+      else {
+        val (i, r0) = r.nextInt
+        loop(n - 1, r0, i :: a)
+      }
+    }
+    loop(count, rng, List.empty)
+  }
+  def randInts(count: Int): Rand[List[Int]] = sequence(List.fill(count)(randInt))
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng0) = ra(rng)
+      val (b, rng1) = rb(rng0)
+      (f(a, b), rng1)
+    }
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List.empty[A]))((f, acc) => map2(f, acc)(_ :: _))
+  }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 
   case class Simple(seed: Long) extends RNG {
     def nextInt: (Int, RNG) = {
@@ -16,43 +89,12 @@ object RNG {
       (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
   }
-
-  type Rand[+A] = RNG => (A, RNG)
-
-  val int: Rand[Int] = _.nextInt
-
-  def unit[A](a: A): Rand[A] =
-    rng => (a, rng)
-
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
-
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
-
-  def double(rng: RNG): (Double, RNG) = ???
-
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
-
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
-
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
-
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
-
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
-
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
-
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
-case class State[S,+A](run: S => (A, S)) {
+case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sys.error("todo")
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     sys.error("todo")
